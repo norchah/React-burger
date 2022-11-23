93a64bf3574f80ee0288cd4b44283db2575a66c5
Anton Kharchenko <norchah@yandex.ru>
2022-11-23 17:22:45 +0200

    fix: some fixing

--- src/constructor-list-elements/list-element.jsx
+++ src/constructor-list-elements/list-element.jsx
@@ -5,7 +5,7 @@ import styles from "./list-element.module.css";
 
 function getFirstElement(item) {
   return (
-    <li key={item._id}className={styles.item}>
+    <li key={item._id} className={styles.item}>
       <ConstructorElement
         type="top"
         isLocked={true}
@@ -34,7 +34,7 @@ function getLastElement(item) {
 function getMiddleElement(item) {
   return (
     <li key={item._id} className={styles.itemMiddle}>
-      <DragIcon type="primary"/>
+      <DragIcon type="primary" />
       <ConstructorElement
         text={item.name}
         price={item.price}
@@ -47,15 +47,24 @@ function getMiddleElement(item) {
 export default function ListElements(props) {
   return (
     <ul className={styles.list}>
-      {props.value.map((item, index) => {
+      {props.value.map((item, index) =>{
         if (index === 0) {
-          return getFirstElement(item);
-        } else if (index === props.value.length - 1) {
-          return getLastElement(item);
-        } else {
+          return getFirstElement(item)
+        }
+      })}
+      <ul className={styles.listMiddle}>
+      {props.value.map((item, index) => {
+        if (index !== 0 && index !== props.value.length - 1) {
           return getMiddleElement(item);
         }
       })}
+      </ul>
+      {props.value.map((item, index) =>{
+        if (index === props.value.length - 1) {
+          return getLastElement(item)
+        }
+      })}
+      
     </ul>
   );
 }
