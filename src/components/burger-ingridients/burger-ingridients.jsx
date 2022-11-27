d74aed4f6b500257824a26aa63b66cfd3144b853
Anton Kharchenko <norchah@yandex.ru>
2022-11-27 15:10:39 +0200

    feat: check prop types

--- src/components/burger-ingridients/burger-ingridients.jsx
+++ src/components/burger-ingridients/burger-ingridients.jsx
@@ -1,15 +1,21 @@
 import React from "react";
+import PropTypes from "prop-types";
 import styles from './burger-ingridients.module.css';
 import Switcher from "../switcher/switcher";
 import Ingridients from "../ingridients/ingridients.jsx";
+import { messagePropTypes } from "../../utils/data";
 
 
-export default function BurgerIngridients(props) {
+export default function BurgerIngridients({value}) {
     return (
         <section className={`${styles.section} mr-10 pt-10`}>
           <h1 className="text text_type_main-large pb-5">Соберите бургер</h1>
           <Switcher/>
-          <Ingridients value={props.value}/>
+          <Ingridients value={value}/>
         </section>
     )
 }
+
+BurgerIngridients.propTypes = {
+  value: PropTypes.arrayOf(messagePropTypes).isRequired
+};
